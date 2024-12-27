3617f22a7b37b88e9845889480d3beade7f82012
Houria Khanjar <gls97048@students.codeinstitute.net>
2024-12-27 21:27:39 +0000

    add substract game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,6 +29,8 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
+    }  else if (gameType === "substract") {
+        displaySubtractQuestion(num1, num2);
     } else {
         alert(`there is no question for operator type ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -73,6 +75,8 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, 'addition'];
     } else if (operator === "X") {
         return [operand1 * operand2, 'multiply'];
+    } else if (operator === "-") {
+        return [operand1 - operand2, 'substract'];
     } else {
         alert(`unimplemented operator ${operator}`);
         throw `unimplemented operator ${operator}, Aborting!`;
@@ -110,8 +114,10 @@ function displayAdditionQuestion(operand1, operand2) {
 
 }
 
-function displaySubtractQuestion() {
-
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1 >= operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 <= operand2 ? operand1 : operand2;
+    document.getElementById("operator").textContent = "-";
 }
 
 function displayMultiplyQuestion( operand1, operand2) {
