a98b639ba3dd0d5b73406b7727e2609c3fd2eb01
Houria Khanjar <gls97048@students.codeinstitute.net>
2024-12-27 20:34:31 +0000

    implement calculate correct answer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,6 +40,16 @@ function checkAnswer() {
 
 function calculateCorrectAnswer() {
 
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
+    if (operator === "+") {
+        return [operand1 + operand2, 'addition'];
+    } else {
+        alert(`unimplemented operator ${operator}`);
+        throw `unimplemented operator ${operator}, Aborting!`;
+    }
+
 }
 
 function incrementScore() {
@@ -51,7 +61,7 @@ function incrementWrongAnswer() {
 }
 
 function displayAdditionQuestion(operand1, operand2) {
-  
+
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "+";
